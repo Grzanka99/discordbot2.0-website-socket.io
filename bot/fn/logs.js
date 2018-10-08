31e0e927c4cd38a3c0ea4a330a3adeb443419431
Cezary Grzanka <cezarygrzanka@gmail.com>
2018-10-08 21:04:22 +0200

    a

--- bot/fn/logs.js
+++ bot/fn/logs.js
@@ -1,9 +1,10 @@
 var fn = require('./fn');
 var io = require('../../app').io;
+var fs = require('fs');
 
 module.exports = function(msg) {
     console.log(fn.gCT()+msg);
     io.emit("log", fn.gCT()+msg);
 
     // console.log(msg) // tymaczasowo
-}
+};
